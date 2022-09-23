f0d0f345c2020b59770e952c397419e80f9ec399
Isabella <wileyisabella@gmail.com>
2022-09-23 18:52:28 -0400

    implemented login feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,31 @@
-function Login(){
+import { useState } from "react";
+
+function Login({ onLogin }){
+    const [name,setName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch(`http://localhost:3000/birders?name=${name}`)
+            .then((r) => r.json())
+            .then(users => {
+                if (users.length > 0) {
+                    onLogin(users[0]);
+                }
+            });
+    }
+
     return(
-        <div>Login</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input 
+                    id="name" 
+                    value={name} 
+                    onChange={e => setName(e.target.value)} 
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
     );
 }
 
